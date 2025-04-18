e5816e74237939fb545fe9be4a20a1f9ba168a86
Savina <30692610+savinamar@users.noreply.github.com>
2025-04-18 22:52:13 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,41 @@
-const toggleButton = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
 
-toggleButton.addEventListener('click', () => {
-  const currentDisplay = window.getComputedStyle(menu).display;
-  menu.style.display = currentDisplay === 'none' ? 'flex' : 'none';
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+  const menu = document.querySelector('.menu');
+  menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
 });
+
+document.addEventListener('click', (event) => {
+  const menu = document.querySelector('.menu');
+  const menuToggle = document.querySelector('.menu-toggle');
+
+  // Αν το κλικ δεν έγινε στο μενού ή στο κουμπί του μενού, το κλείνουμε
+  if (!menu.contains(event.target) && event.target !== menuToggle) {
+    menu.style.display = 'none';
+  }
+});
+
+// για το μενού να κλείνει
+document.querySelectorAll('.menu a').forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelector('.menu').style.display = 'none';
+  });
+});
+
+
+
+
+//top 
+
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
+
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
