e8ca8446d1422462b18f8a3cbedf7103fb55268f
Savina <30692610+savinamar@users.noreply.github.com>
2025-04-18 21:23:42 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,6 @@
-<script>
   const toggleButton = document.querySelector('.menu-toggle');
   const menu = document.querySelector('.menu');
 
   toggleButton.addEventListener('click', () => {
     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
   });
-</script>
